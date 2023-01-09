7c5c47b49c098200957df8f17215a0fe54fa6f19
A0X0E0L <ax13xxi@gmail.com>
2023-01-09 20:53:17 +0000

    Semaforo terminado usando Hooks que al final ni use

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const [color1,setColor1]= useState("");
+	const [sombra1,setSombra1] = useState("");
+	function encenderRojo() {
+
+		if(color1 === "btn-danger"){
+			setColor1("")
+			
+		}else{
+			setColor2("")
+			setColor3("")
+		setColor1("btn-danger")
+		setSombra1("0 0 15px 10px red");
+	}}
+	const [color2,setColor2]= useState("");
+	
+	function encenderAmarillo() {
+		if(color2 === "btn-warning"){
+		setColor2("")
+		}else{
+		setColor3("")
+		setColor1("")
+		setColor2("btn-warning")
+		
+	}};
+	const [color3,setColor3]= useState("");
+	const [sombra2,setSombra2] = useState("");
+	function encenderVerde() {
+		if(color3 === "btn-success"){
+		setColor3("")
+		}else{
+			setColor2("")
+			setColor1("")
+		setColor3("btn-success")
+		setSombra1
+		setSombra2
+	}};
+return (
+
+	<div id="semaforo" className="d-flex flex-column mx-auto ">
+		<button className={"btn  "+color1} id="Rojo" style={{boxShadow: sombra1}} onClick={encenderRojo}></button>
+		<button className={"btn "+color2} id="Amarillo" onClick={encenderAmarillo}></button>
+		<button className={"btn "+color3} id= "Verde"onClick={encenderVerde}></button>
+	</div>
+	
+);
 };
+
 
 export default Home;
